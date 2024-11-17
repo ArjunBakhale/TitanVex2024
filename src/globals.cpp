b6f6a9ba74c5b56f115f432257ad10ea255af149
Arjun Bakhale <arjun.bakhale@gmail.com>
2024-11-16 20:46:34 -0500

    798

--- src/globals.cpp
+++ src/globals.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+// TODO: Update globals
 // pros::ADIDigitalOut wings ('G');
 // pros::Motor Intake(1, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 // pros::Motor Slapper(11, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
