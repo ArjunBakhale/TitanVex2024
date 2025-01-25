da436c451d7d561bac4ea3fd3a4d730f5418d994
Akshit Kumar <akshit2k26@gmail.com>
2025-01-24 21:28:00 -0500

    update hyperparmaer

--- src/autons.cpp
+++ src/autons.cpp
@@ -18,7 +18,13 @@ const int SWING_SPEED = 90;
 ///
 // Constants
 ///
-
+//kP is 15 and the kD is 300
+/*
+notes: 
+do the turn first
+find a good Kp AND THEN A GOOD kD for turn
+then do heading: adjust kP until it barely veers into a certain direction and then stop: it hsoul look look undesireable before we mess with kD
+s*/
 // It's best practice to tune constants when the robot is empty and with heavier game objects, or with lifts up vs down.
 // If the objects are light or the cog doesn't change much, then there isn't a concern here.
 
@@ -29,15 +35,20 @@ void default_constants() {
   chassis.set_pid_constants(&chassis.backward_drivePID, 2.5, 0.5, 5, 0);*/
 
 //untuned
-  chassis.set_pid_constants(&chassis.forward_drivePID, 3.95, 0, 3, 0);
+  
+  //chassis.set_pid_constants(&chassis.forward_drivePID, 25, 0, 0, 0);
 
 
   // chassis.set_pid_constants(&chassis.backward_drivePID, 0.35, 0, 5, 0);
-  // chassis.set_pid_constants(&chassis.headingPID, 8, 0, 20, 0);
+  //chassis.set_pid_constants(&chassis.headingPID, 8, 0, 20, 0);
   // chassis.set_pid_constants(&chassis.turnPID, 7.5, 0.001, 79, 15);
   // chassis.set_pid_constants(&chassis.swingPID, 5, 0.002, 79, 15);
 
+  chassis.set_pid_constants(&chassis.forward_drivePID, 15, 0, 5, 0);
   //tuned
+  chassis.set_pid_constants(&chassis.turnPID, 5, 0, 50, 15);
+  chassis.set_pid_constants(&chassis.headingPID, 11, 0, 20, 0);
+  chassis.set_pid_constants(&chassis.swingPID, 7, 0, 45, 0);
 }
 
 // void one_mogo_constants() {
@@ -72,7 +83,7 @@ void pid_tune() {
   chassis.set_drive_pid(60, DRIVE_SPEED, true);
   chassis.wait_drive();
 
-  // chassis.set_turn_pid(45, TURN_SPEED);
+  // chassis.set_turn_pid(90, TURN_SPEED);
   // chassis.wait_drive();
 
   // chassis.set_swing_pid(ez::RIGHT_SWING, 45, SWING_SPEED);
